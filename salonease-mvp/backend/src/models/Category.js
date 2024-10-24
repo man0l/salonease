950b7da3d29ab2981ae2af75770ce352fd7118f4
Manol Trendafilov <manol@camplight.dev>
2024-10-24 12:38:33 +0300

    fix model relation

--- salonease-mvp/backend/src/models/Category.js
+++ salonease-mvp/backend/src/models/Category.js
@@ -29,7 +29,7 @@ module.exports = (sequelize) => {
       type: DataTypes.INTEGER,
       allowNull: true,
       references: {
-        model: 'Categories',
+        model: 'Category',
         key: 'id',
       },
     },
