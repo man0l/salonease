c257f24c7f243c94ad546efa4bb35f4d3b64b671
Manol Trendafilov <manol@camplight.dev>
2025-01-11 13:33:39 +0200

    add ssl version

--- salonease-mvp/backend/src/utils/helpers/emailHelper.js
+++ salonease-mvp/backend/src/utils/helpers/emailHelper.js
@@ -9,6 +9,10 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+    ciphers: 'SSLv3',
+    rejectUnauthorized: false
+  }
 });
 
 const sendEmail = async (to, subject, htmlContent, textContent) => {
