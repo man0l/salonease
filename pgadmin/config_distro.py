b553d975c73a2708b8c47c689efc5ebd3b1b8743
Manol Trendafilov <manol@camplight.dev>
2025-01-11 15:42:13 +0200

    Refactor pgAdmin service configuration in Docker Compose
    - Replaced outdated CORS middleware with new custom headers middleware for improved request handling and security.
    - Introduced proxy-related settings in the pgAdmin configuration to enhance cookie security and compliance.
    - Streamlined middleware usage to focus on essential configurations, improving overall clarity and maintainability.

--- pgadmin/config_distro.py
+++ pgadmin/config_distro.py
@@ -1,3 +1,9 @@
 X_CONTENT_TYPE_OPTIONS = ""    # default value is nosniff
 ENHANCED_COOKIE_PROTECTION = False
 X_XSS_PROTECTION = "0"  # default value is '1; mode=block'
+# Proxy-related settings
+SECURE_PROXY_SSL_HEADER = ('X-Forwarded-Proto', 'https')
+CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
